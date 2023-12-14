a202cb6994dd7bcfdd3a2f9a812ced5e79aaa34c
Aktan Klimov <aktan.klimov2@gmail.com>
2023-12-14 14:22:13 +0600

    Product enum

---
+++ src/emums/Product.java
@@ -0,0 +1,29 @@
+package emums;
+
+public class Product {
+    private Quality quality;
+
+    public Product(Quality quality) {
+        this.quality = quality;
+    }
+
+    public void decreaseQuality() {
+        switch (quality) {
+            case EXCELLENT:
+                quality = Quality.GOOD;
+                break;
+            case GOOD:
+                quality = Quality.AVERAGE;
+                break;
+            case AVERAGE:
+                quality = Quality.POOR;
+                break;
+            case POOR:
+                break;
+        }
+    }
+
+    public Quality getQuality() {
+        return quality;
+    }
+}
