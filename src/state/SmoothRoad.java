096e0f63f914a8746d5de8cdeb383601a2a0692b
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 15:00:37 +0600

    implemented event SmoothRoad

--- src/state/SmoothRoad.java
+++ src/state/SmoothRoad.java
@@ -5,6 +5,11 @@ import model.Trader;
 public class SmoothRoad implements Event {
     @Override
     public void run(Trader trader) {
+        System.out.println(" отличная дорога  скорость увеличена на две лиги ( но не более пяти лиг.) ");
+        trader.setSpeed(trader.getSpeed() + 2);
+        if (trader.getSpeed() > 5) {
+            trader.setSpeed(5);
+        }
 
     }
 }
