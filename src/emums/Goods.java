a2dd1cb3803330960672ebaa90262f7801d7daf7
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 16:18:13 +0600

    changed enum good, added start price

--- src/emums/Goods.java
+++ src/emums/Goods.java
@@ -1,12 +1,12 @@
 package emums;
 
 public enum Goods {
-    MEAT(10, 1.2,0),
-    DRIED_FRUITS(20, 1.2,0),
-    GRAIN(30, 1.2,0),
-    FLOUR(30, 1.2,0),
-    CLOTH(20, 1.2,0),
-    PAINT(50, 1.2,0);
+    MEAT(10, 1.2,1000),
+    DRIED_FRUITS(20, 1.2,250),
+    GRAIN(30, 1.2,400),
+    FLOUR(30, 1.2,500),
+    CLOTH(20, 1.2,700),
+    PAINT(50, 1.2,800);
 
 
     private final int weight;
@@ -27,5 +27,9 @@ public enum Goods {
 
     public double getQualityCoef() {
         return qualityCoef;
+    }
+
+    public int getPriceOnPoint() {
+        return priceOnPoint;
     }
 }
