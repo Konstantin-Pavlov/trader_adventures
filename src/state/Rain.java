43da52ba73a103e9813d78dfc07b371f47c382c8
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 14:48:55 +0600

    implemented event When it started to rain

--- src/state/Rain.java
+++ src/state/Rain.java
@@ -2,9 +2,15 @@ package state;
 
 import model.Trader;
 
+import java.util.Random;
+
 public class Rain implements Event {
     @Override
     public void run(Trader trader) {
-
+        System.out.println("сегодня идет дождь, скорость снижеа на две лиги ");
+        trader.setSpeed(trader.getSpeed()-2);
+        if (new Random().nextInt(10) < 3){
+            System.out.println("с вероятностью 30% товар испортился качестово и цена снижеы");
+        }
     }
 }
