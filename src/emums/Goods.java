6f0f8cc5527db12acf68253696218c4d9f37dbe6
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 17:54:27 +0600

    added methods to goods enum

--- src/emums/Goods.java
+++ src/emums/Goods.java
@@ -1,35 +1,46 @@
 package emums;
 
 public enum Goods {
-    MEAT(10, 1.2,1000),
-    DRIED_FRUITS(20, 1.2,250),
-    GRAIN(30, 1.2,400),
-    FLOUR(30, 1.2,500),
-    CLOTH(20, 1.2,700),
-    PAINT(50, 1.2,800);
+    MEAT(10, Quality.NORMAL.getQualityCoefficient(), 1000),
+    DRIED_FRUITS(20, Quality.NORMAL.getQualityCoefficient(), 250),
+    GRAIN(30, Quality.NORMAL.getQualityCoefficient(), 400),
+    FLOUR(30, Quality.NORMAL.getQualityCoefficient(), 500),
+    CLOTH(20, Quality.NORMAL.getQualityCoefficient(), 700),
+    PAINT(50, Quality.NORMAL.getQualityCoefficient(), 800);
 
 
     private final int weight;
-    private final double qualityCoef;
-    private int priceOnPoint;
+    private double qualityCoefficient;
+    private final int priceOnPoint;
+    private Quality quality;
 
-    Goods(int weight, double qualityCoef, int priceOnPoint) {
+    Goods(int weight, double qualityCoefficient, int priceOnPoint) {
         this.weight = weight;
-        this.qualityCoef = qualityCoef;
+        this.qualityCoefficient = qualityCoefficient;
         this.priceOnPoint = priceOnPoint;
+        this.quality = Quality.NORMAL;
     }
-
 
 
     public int getWeight() {
         return weight;
     }
 
-    public double getQualityCoef() {
-        return qualityCoef;
+    public double getQualityCoefficient() {
+        return qualityCoefficient;
+    }
+
+    public double getFinalPrice() {
+        return this.priceOnPoint * this.quality.getQualityCoefficient();
     }
 
     public int getPriceOnPoint() {
         return priceOnPoint;
     }
+
+    public void setQuality(Quality quality) {
+        this.quality = quality;
+        this.qualityCoefficient = quality.getQualityCoefficient();
+    }
+
 }
