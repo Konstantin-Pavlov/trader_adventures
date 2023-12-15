f11134b1292c560d877387c349785e55c5cf676c
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 17:33:00 +0600

    fields in the enum Quality were changed in accordance with the task, coefficient values were added to values

--- src/emums/Quality.java
+++ src/emums/Quality.java
@@ -2,8 +2,20 @@ package emums;
 
 public enum Quality
 {
-    POOR,
-    AVERAGE,
-    GOOD,
-    EXCELLENT
+    NORMAL (1.2),
+    SLIGHTLY_DAMAGED( 0.95),
+    HALF_DAMAGED (0.55),
+    ALMOST_DAMAGED(0.25),
+    COMPLETELY_RUINED( 0.1);
+
+
+    private double qualityCoefficient;
+
+    Quality(double qualityCoefficient) {
+        this.qualityCoefficient = qualityCoefficient;
+    }
+
+    public double getQualityCoefficient() {
+        return qualityCoefficient;
+    }
 }
