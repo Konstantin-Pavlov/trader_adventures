3f7d6ced55b0dcf0bbd9848dd2077f7f5e5fc40d
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 16:04:37 +0600

    implemented event NormalDay

--- src/state/NormalDay.java
+++ src/state/NormalDay.java
@@ -3,12 +3,8 @@ package state;
 import model.Trader;
 
 public class NormalDay implements Event {
-    public void printNormal(){
-        System.out.println("Normal day, all right!");
-    }
-
     @Override
     public void run(Trader trader) {
-
+        System.out.println("Обычный день - ничего не произошло.");
     }
 }
