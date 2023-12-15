16831d6a7ff9f0dd4dfea40ed9dc11a609b108b2
Mariyanail88 <mariasherbakiva@mail.ru>
2023-12-15 18:42:14 +0600

    implemented a decrease in the quality of a randomly selected product

--- src/state/Rain.java
+++ src/state/Rain.java
@@ -1,5 +1,7 @@
 package state;
 
+import emums.Goods;
+import emums.Quality;
 import model.Trader;
 
 import java.util.Random;
@@ -10,7 +12,28 @@ public class Rain implements Event {
         System.out.println("сегодня идет дождь, скорость снижеа на две лиги ");
         trader.setSpeed(trader.getSpeed()-2);
         if (new Random().nextInt(10) < 3){
-            System.out.println("с вероятностью 30% товар испортился качестово и цена снижеы");
+            System.out.println("с вероятностью 30% 1 товар испортился качество и цена снижены");
+            Goods good = trader.getRandomGood();
+            switch (good.getQuality()) {
+                case NORMAL:
+                    good.setQuality(Quality.SLIGHTLY_DAMAGED);
+                    break;
+                case SLIGHTLY_DAMAGED:
+                    good.setQuality(Quality.HALF_DAMAGED);
+                    break;
+                case HALF_DAMAGED:
+                    good.setQuality(Quality.ALMOST_DAMAGED);
+                    break;
+                case ALMOST_DAMAGED:
+                    good.setQuality(Quality.COMPLETELY_RUINED);
+                    break;
+                default:
+                    System.err.println("товар максимально испорчен");
+
+
+            }
+
+            }
         }
     }
 }
