ca572ba123abfee9d6538aadc14fdfdb732b60e6
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-17 21:43:17 +0600

    implement Bandits event

--- src/state/Bandits.java
+++ src/state/Bandits.java
@@ -1,11 +1,33 @@
 package state;
 
+import emums.Goods;
 import interfaces.Event;
 import model.Trader;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 public class Bandits implements Event {
     @Override
     public void run(Trader trader) {
+        System.out.println("it's a robbery!");
+        if (new Random().nextBoolean()) {
+            System.out.printf("you  have %d money, we take it", trader.getTotalCash());
+            trader.setTotalCash(0);
+        } else {
+            banditsGetTheBestGood(trader.getPurchasedGoods());
+        }
+    }
+
+    private void banditsGetTheBestGood(List<Goods> goods) {
+        if (goods == null) {
+            System.out.println("there's no goods");
+            return;
+        }
+        Goods goodWithMaxPrice = goods.stream().max(Comparator.comparing(Goods::getFinalPrice)).orElse(null);
+        System.out.println("украден один из самых дорогих товаров: " + goodWithMaxPrice);
+        goods.remove(goodWithMaxPrice);
 
     }
 }
