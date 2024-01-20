92df006d03476e6ccc10bbf6d17bdbad7a37fd66
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-01-20 22:04:40 +0600

    add color to message

--- src/state/Bandits.java
+++ src/state/Bandits.java
@@ -7,11 +7,12 @@ import model.Trader;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import util.ConsoleColors;
 
 public class Bandits implements Event {
     @Override
     public void run(Trader trader) {
-        System.out.println("it's a robbery!");
+        System.out.println(ConsoleColors.ANSI_RED_BACKGROUND + "it's a robbery!" + ConsoleColors.ANSI_RESET);
         if (new Random().nextBoolean()) {
             System.out.printf("you  have %.2f money, we take it%n", trader.getTotalCash());
             trader.setTotalCash(0);
